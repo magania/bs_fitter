71e243518ecaeac7446890f5593eb417dbb959bc
magania <magania@box.(none)>
2009-05-19 15:23:18 -0500

    Correction of angles in pull.

--- bin/toy/pull.C
+++ bin/toy/pull.C
@@ -1,4 +1,36 @@
-plotVar(const char* var, RooRealVar &RR, const char* cuts ,TTree &fits){
+Double_t PI = 3.141592654;
+Double_t PI2 = 1.570796327;
+
+void correctAngle(Double_t* angle){
+	if ( (*angle) > -PI  && (*angle) < PI )
+		return;
+	if ( (*angle) < -PI )
+		(*angle) = 2*PI+(*angle);
+	else
+		(*angle) = -2*PI+(*angle);
+	correctAngle(angle);
+}
+
+bool transformDG(Double_t *DG, Double_t *Phi_s, Double_t *Delta_1, Double_t *Delta_2){
+	if ( (*DG)*cos(*Phi_s) < 0 )
+		return false;
+
+	if ((*DG)<0 ) {
+		(*DG) = -(*DG);
+		(*Phi_s) = PI-(*Phi_s);
+		(*Delta_1) = PI-(*Delta_1);
+		(*Delta_1) = PI-(*Delta_2);
+	}
+
+	correctAngle(Phi_s);
+	correctAngle(Delta_1);
+	correctAngle(Delta_2);
+
+	return true;
+}
+
+
+void plotVar(const char* var, RooRealVar &RR, const char* cuts ,TTree &fits){
  stringstream ss, ss_canvas, ss_var, ss_hvar, ss_pull, ss_evar, ss_eevar;
  ss_var << var << " >> hvar_" << var << "(50," << RR.getVal()-5*RR.getError() << "," << RR.getVal()+5*RR.getError() <<")";
  ss_evar  << 'e' << var << " >> hvar_e" << var << "(50,0," << 3*RR.getError() << ")";
@@ -13,7 +45,7 @@ plotVar(const char* var, RooRealVar &RR, const char* cuts ,TTree &fits){
  canvas->cd(1);
  cout << ss_var.str() << endl;
  fits.Draw(ss_var.str().c_str(), cuts);
- ((TH1*) gROOT->Get(ss_hvar.str().c_str()))->Fit("gaus"); 
+ ((TH1*) gROOT->Get(ss_hvar.str().c_str()))->Fit("gaus");
  ((TH1*) gROOT->Get(ss_hvar.str().c_str()))->SetTitle(var);
  TArrow *flecha = new TArrow(RR.getVal(), 30, RR.getVal(),0, 0.01,">");
  flecha->Draw();
@@ -22,9 +54,9 @@ plotVar(const char* var, RooRealVar &RR, const char* cuts ,TTree &fits){
  Double_t mean_error = fit->GetParError(1);
  Double_t sigma = fit->GetParameter(2);
  Double_t sigma_error = fit->GetParError(2);
- wiki << "|**" << RR.GetName() << "** | " 
-      << RR.getVal() << " +/- " << RR.getError() << " | " 
-      << mean << " +/- " << mean_error << " | " 
+ wiki << "|**" << RR.GetName() << "** | "
+      << RR.getVal() << " +/- " << RR.getError() << " | "
+      << mean << " +/- " << mean_error << " | "
       << sigma << " +/- " << sigma_error << " | ";
 
  canvas->cd(2);
@@ -46,45 +78,71 @@ plotVar(const char* var, RooRealVar &RR, const char* cuts ,TTree &fits){
  wiki << mean << " +/- " << mean_error << " | "
       << sigma << " +/- " << sigma_error << " | "<< endl;
 
-
 }
 
 
 stringstream wiki;
 
-pull(){
+void pull(){
  gROOT->SetStyle("Plain");
  gStyle->SetOptStat("000000000");
  gStyle->SetOptFit(1011);
  gStyle->SetStatW(0.3);
 
- RooRealVar A0("A0", "A0", 0);
- RooRealVar A1("A1", "A1", 0);
- RooRealVar DeltaGamma("DeltaGamma", "#Delta#Gamma", 0);
- RooRealVar Delta_1("Delta_1", "#delta_{1}", 0);
- RooRealVar Delta_2("Delta_2", "#delta_{2}", 0);
- RooRealVar Phi_s("Phi_s", "#phi_{s}", 0);
- RooRealVar Tau("Tau", "#tau", 0);
- RooRealVar DeltaMs("DeltaMs", "#Delta M_{s}", 0);
+ RooRealVar rA0("A0", "A0", 0);
+ RooRealVar rA1("A1", "A1", 0);
+ RooRealVar rDeltaGamma("DeltaGamma", "#Delta#Gamma", 0);
+ RooRealVar rDelta_1("Delta_1", "#delta_{1}", 0);
+ RooRealVar rDelta_2("Delta_2", "#delta_{2}", 0);
+ RooRealVar rPhi_s("Phi_s", "#phi_{s}", 0);
+ RooRealVar rTau("Tau", "#tau", 0);
+ //RooRealVar DeltaMs("DeltaMs", "#Delta M_{s}", 0);
 
  RooArgSet parameters(A0,A1,DeltaGamma,Delta_1, Delta_2, Phi_s, Tau);
- parameters.readFromFile("../parameters.txt"); 
+ parameters.readFromFile("../parameters.txt");
 
+ Double_t A0,eA0,A1,eA1,DG,eDG,Delta_1,eDelta_1,Delta_2,eDelta_2,Phi_s,ePhi_s,Tau,eTau;
  TTree fits("fits","fits");
- cout << "Read: " 
-      << fits.ReadFile("fits.dat","A0/D:eA0:A1:eA1:DeltaGamma:eDeltaGamma:Delta_1:eDelta_1:Delta_2:eDelta_2:Phi_s:ePhi_s:Tau:eTau") 
-      << endl;
+ fits.Branch("A0", &A0, "A0");
+ fits.Branch("eA0", &eA0, "eA0");
+ fits.Branch("A1", &A1, "A1");
+ fits.Branch("eA1", e&A1, "eA1");
+ fits.Branch("DeltaGamma", &DG, "DeltaGamma");
+ fits.Branch("eDeltaGamma", &eDG, "eDeltaGamma");
+ fits.Branch("Delta_1", &Delta_1, "Delta_1");
+ fits.Branch("eDelta_1", &eDelta_1, "eDelta_1");
+ fits.Branch("Delta_2", &Delta_2, "Delta_2");
+ fits.Branch("eDelta_2", &eDelta_2, "eDelta_2");
+ fits.Branch("Phi_s", &Phi_s, "Phi_s");
+ fits.Branch("ePhi_s", &ePhi_s, "ePhi_s");
+ fits.Branch("Tau", &Tau, "Tau");
+ fits.Branch("eTau", &eTau, "eTau");
 
- const char * cuts = "Phi_s>-3.141592654 && Phi_s<3.141592654 && Delta_1>-2*3.141592654 && Delta_1<2*3.141592654 && Delta_2>-3.141592654 && Delta_2<3*3.141592654";
- plotVar("A0", A0, cuts, fits);
- plotVar("A1", A1, cuts, fits);
- plotVar("DeltaGamma", DeltaGamma, cuts, fits);
- plotVar("Delta_1", Delta_1, cuts, fits);
- plotVar("Delta_2", Delta_2, cuts, fits);
- plotVar("Phi_s", Phi_s, cuts, fits);
- plotVar("Tau", Tau, cuts, fits);
+
+ ifstream fitdat("fits.dat");
+ int total = 0;
+ int corrected = 0;
+ while (!fitdat.eof()){
+	 fitdat >> A0 >> eA0 >> A1 >> eA1 >> DG >> eDG >> Delta_1 >> eDelta_1 >> Delta_2 >> eDelta_2 >> Phi_s >> ePhi_s >> Tau >> eTau;
+	 total++;
+	 if (transformDG(*DG, *Phi_s, *Delta_1, *Delta_2)){
+		 tree.Fill();
+		 corrected++;
+	 }
+ }
+
+
+ const char * cuts = "";
+ plotVar("A0", rA0, cuts, fits);
+ plotVar("A1", rA1, cuts, fits);
+ plotVar("DeltaGamma", rDeltaGamma, cuts, fits);
+ plotVar("Delta_1", rDelta_1, cuts, fits);
+ plotVar("Delta_2", rDelta_2, cuts, fits);
+ plotVar("Phi_s", rPhi_s, cuts, fits);
+ plotVar("Tau", rTau, cuts, fits);
 
  cout << endl;
+ cout << "Total fits: " << total << " Corrected fits: " << corrected << endl;
  cout << "^ **Parameter** ^ **Generated Value** ^ **Parameter Mean** ^ **Parameter Sigma** ^ **Pull Mean** ^ **Pull Sigma** ^" << endl;
  cout << wiki.str() << endl;
 }
